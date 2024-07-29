82ccdadce1d79c981a1d0ade8cc036438355bc33
Rishi <rishirjjoshi@gmail.com>
2024-07-29 19:49:52 +0530

    .

--- SEM 5/python/File_Handling/22_file_metadata.py
+++ SEM 5/python/File_Handling/22_file_metadata.py
@@ -1,14 +1,22 @@
+import os
+
+def occurrences(data_file, char_to_find):
+    return data_file.count(char_to_find)
+
 try:
-    file_path = "Text_files/22_Data.txt"  # Relative path to the file
-    file_obj = open(file_path, "r")
-    text = file_obj.read()
-    print(text)
-    
+    current_dir = os.path.dirname(__file__)    
+    file_path = os.path.join(current_dir, "Text_files", "22_Data.txt")
+
+    with open(file_path, "r") as file_obj:
+        data = file_obj.read()  # * data will contains all data
+        words = len(data.split()) 
+        lines = len(data.splitlines())
+        
+        print(f"Word Count: {words}")
+        print(f"Lines Count: {lines}")
+        print(f"Word Occured: {occurrences(data, "dolor")}")
+        print(f"Char Occured: {occurrences(data, "e")}")
+        print(f"Blank Spaces: {occurrences(data, " ")}")
+
 except FileNotFoundError:
     print("File is not found")
-finally:
-    # Ensure file is closed if it was successfully opened
-    try:
-        file_obj.close()
-    except NameError:
-        pass  # file_obj was not defined because the file could not be opened
