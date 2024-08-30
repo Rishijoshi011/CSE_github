be808d8a3530fa6b815894feadb7898d928f416b
Rishi <rishirjjoshi@gmail.com>
2024-08-30 22:29:49 +0530

    Update 50_image.py

--- SEM 5/python/11_Tkinter/50_image.py
+++ SEM 5/python/11_Tkinter/50_image.py
@@ -22,21 +22,21 @@ lb_email.grid(row = 2, column = 0, padx = 10, pady = 5)
 inp_email = tk.Entry(window)
 inp_email.grid(row = 2, column = 1, padx = 10, pady = 5)
 
-lb_result = tk.Label(window, text="")
+lb_result = tk.Label(window, text = "")
 lb_result.grid(row = 4, column = 1, pady = 5)
 
-lb_gender = tk.Label(window, text="Gender")
+lb_gender = tk.Label(window, text = "Gender")
 lb_gender.grid(row = 3, column = 0, padx = 10, pady = 5)
 
 gender_var = tk.StringVar(value = "Male")
 
-male = tk.Radiobutton(window, text="Male", variable = gender_var, value = "Male")
+male = tk.Radiobutton(window, text = "Male", variable = gender_var, value = "Male")
 male.grid(row = 3, column = 1, padx = 10, pady = 5)
 
-female = tk.Radiobutton(window, text="Female", variable = gender_var, value = "Female")
+female = tk.Radiobutton(window, text = "Female", variable = gender_var, value = "Female")
 female.grid(row = 3, column = 2, padx = 10, pady = 5)
 
-btn_login = tk.Button(window, text="Register")
+btn_login = tk.Button(window, text = "Register")
 btn_login.grid(row = 4, column = 1, pady = 10)
 
 window.mainloop()
