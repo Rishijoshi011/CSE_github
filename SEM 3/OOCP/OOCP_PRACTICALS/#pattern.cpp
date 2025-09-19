16320f69acc343f8b2e0e9a133df13f39f5f6065
Rishi <rishirjjoshi@gmail.com>
2025-09-19 23:23:06 +0530

    Create #pattern.cpp
    .

---
+++ SEM 3/OOCP/OOCP_PRACTICALS/#pattern.cpp
@@ -0,0 +1,40 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+// ! left, middle and right triangle pattern is dependent on the space given in ""
+// * left triangle space = ""
+// * middle triangle space = " "
+// * right triangle space = "  "
+    
+int main() {
+
+    int k = 5;
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5; j++)
+        {
+            if (j >= k) { cout << "* "; }
+            else { cout << ""; }
+
+        }
+        cout << endl;
+        k--;
+    }
+
+    k = 1;
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            if (j >= k) { 
+                cout << "* "; 
+            } else { 
+                cout << ""; 
+            }
+        }
+        cout << endl;
+        k++;
+    }
+
+    return 0;
+}
